1d0f5277f69cdb0d7c6a5ddee496e7c8390b28f3
PRASHANTH29NARASIMHAN <prashanthshanthu007@gmail.com>
2024-05-19 13:48:09 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
-st.title('CRIMINAL DETECTION')
 from PIL import Image
-with Image.open('https://drive.google.com/file/d/1aUc_XtCjoy1Zv-ZsJbucBr-XY45UhEiX/view?usp=sharing') as img:
-    img.show()
+st.title('CRIMINAL DETECTION')
+phn = Image.open(r"https://drive.google.com/file/d/1aUc_XtCjoy1Zv-ZsJbucBr-XY45UhEiX/view?usp=sharing")
+st.set_page_config(page_title="PhonePe Pulse", page_icon=phn, layout="wide")
+
 query = st.text_input('Enter the name of the criminal')
 query = st.number_input('Enter the crimeId of the criminal')
 start_date = st.date_input('Crime committed date', value=pd.Timestamp('2021-01-01'))

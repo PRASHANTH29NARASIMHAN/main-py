56369a851797d3f843057ff175288aaf4f158bed
PRASHANTH29NARASIMHAN <prashanthshanthu007@gmail.com>
2024-05-19 14:10:17 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 st.title('CRIMINAL DETECTION')
-phn = Image.open(r"https://drive.google.com/file/d/1aUc_XtCjoy1Zv-ZsJbucBr-XY45UhEiX/view?usp=sharing")
+phn = Image.open(r"https://drive.google.com/file/d/1aUc_XtCjoy1Zv-ZsJbucBr-XY45UhEiX/view?usp=drive_link")
 st.set_page_config(page_title="PhonePe Pulse", page_icon=phn, layout="wide")
 
 query = st.text_input('Enter the name of the criminal')

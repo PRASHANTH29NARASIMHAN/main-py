4c82053b8d5969f1a733ceec3cd3cde3747ab342
PRASHANTH29NARASIMHAN <prashanthshanthu007@gmail.com>
2024-05-19 14:12:51 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -6,12 +6,14 @@ import json
 
 st.title('CRIMINAL DETECTION')
 
-phn = Image.open(r"E:\criminal identification face detection\criminal detection\criminal.png")
-st.set_page_config(page_title="PhonePe Pulse", page_icon=phn, layout="wide")
-main_path = r"E:\criminal identification face detection\criminal detection"
+
 
 query = st.text_input('Enter the name of the criminal')
 query = st.number_input('Enter the crimeId of the criminal')
 start_date = st.date_input('Crime committed date', value=pd.Timestamp('2021-01-01'))
 num_tweets = st.number_input('Number of cases filed on the crminal', min_value=1, max_value=1000)
 submit = st.button('Search for crminal details')
+
+phn = Image.open(r"E:\criminal identification face detection\criminal detection\criminal.png")
+st.set_page_config(page_title="PhonePe Pulse", page_icon=phn, layout="wide")
+main_path = r"E:\criminal identification face detection\criminal detection"

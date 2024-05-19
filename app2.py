cfbe3048fa43021f5454287603aed29a3f7572fa
PRASHANTH29NARASIMHAN <prashanthshanthu007@gmail.com>
2024-05-19 13:19:25 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('CRIMINAL DETECTION')
 query = st.text_input('Enter the name of the criminal')
-query = st.number_input('Enter the crimeId of the criminal')
+query = st.number_input('Enter the crime Id of the criminal')
 start_date = st.date_input('Crime committed date', value=pd.Timestamp('2021-01-01'))
 num_tweets = st.number_input('Number of cases filed on the crminal', min_value=1, max_value=1000)
 submit = st.button('Search for crminal details')
